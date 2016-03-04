5c9c5875fdd8c67e218a73cd7471eaa1ac7ffb58
Francisco X. Alvarez <franciscoalvarezd@gmail.com>
2016-03-04 14:56:54 -0800

    base ready

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,21 +7,16 @@
         var vm = this;
 
         vm.awesomeThings = [];
-        vm.classAnimation = '';
         vm.showToastr = showToastr;
 
         activate();
 
         function activate() {
             getWebDevTec();
-            $timeout(function() {
-                vm.classAnimation = 'rubberBand';
-            }, 4000);
         }
 
         function showToastr() {
-            toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-            vm.classAnimation = '';
+            toastr.info('Something happened!');
         }
 
         function getWebDevTec() {

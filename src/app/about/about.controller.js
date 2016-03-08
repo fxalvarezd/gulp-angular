5e038b4c410bda731f432d2d52b6eab0af083510
Francisco X. Alvarez <franciscoalvarezd@gmail.com>
2016-03-07 16:40:22 -0800

    fix to localstorage

--- src/app/about/about.controller.js
+++ src/app/about/about.controller.js
@@ -8,15 +8,42 @@
 
         // Bindables
         vm.steps = [
-            'Add "Contact" page.',
-            'Set proper navbar item as active.',
-            'Create a Controller for the "Contact" page.',
-            'Set the "Contact" page title from controller.',
-            'Inside the "Contact" page, create a responsive grid like the one shown in the image below.',
-            'Use the markup from the "Sidebar Nav" panel below inside the "Content 1" box.',
-            'Use Nested Routing to display content for each of the sections inside the "Contact" page.',
-            'Create a "contact" directive, and include it inside each of the sections.',
-            'Create a service that retrieves data from test/contacts.json.'
+            {
+                description: 'Add "Contact" page.',
+                stored: false
+            },
+            {
+                description: 'Set proper navbar item as active.',
+                stored: false
+            },
+            {
+                description: 'Create a Controller for the "Contact" page.',
+                stored: false
+            },
+            {
+                description: 'Set the "Contact" page title from controller.',
+                stored: false
+            },
+            {
+                description: 'Inside the "Contact" page, create a responsive grid like the one shown in the image below.',
+                stored: false
+            },
+            {
+                description: 'Use the markup from the "Sidebar Nav" panel below inside the "Content 1" box.',
+                stored: false
+            },
+            {
+                description: 'Use Nested Routing to display content for each of the sections inside the "Contact" page.',
+                stored: false
+            },
+            {
+                description: 'Create a "contact" directive, and include it inside each of the sections.',
+                stored: false
+            },
+            {
+                description: 'Create a service that retrieves data from test/contacts.json.',
+                stored: false
+            }
         ];
         vm.toggleStorage = toggleStorage;
 
@@ -25,14 +52,22 @@
         // Functions
         function init() {
             if( angular.isDefined($localStorage.steps) ) {
-                vm.storage = $localStorage.steps;
+                for (var i = 0; i < $localStorage.steps.length; i++) {
+                    if ($localStorage.steps[i]) {
+                        vm.steps[i].stored = true;
+                    }
+                }
             } else {
                 $localStorage.steps = [];
             }
         }
 
-        function toggleStorage(index) {
-            $localStorage.steps[index] = true;
+        function toggleStorage(index, check) {
+            if (check) {
+                $localStorage.steps[index] = true;
+            } else {
+                $localStorage.steps[index] = false;
+            }
         }
     }
 })();
